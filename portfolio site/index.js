0b25fd8c224b4d78174d76d08bb1012eb6451ef9
Titobiloluwa Tella <titobiloluwatella@gmail.com>
2022-11-04 18:58:31 +0100

    Add files via upload

---
+++ portfolio site/index.js
@@ -0,0 +1,16 @@
+var sideMenu = document.getElementById("sidemenu");
+
+function openMenu(){
+  sideMenu.style.right = "0";
+}
+function closeMenu(){
+  sideMenu.style.right ="-200px";
+}
+// addEventListener('click',()=>{
+//   if(sideMenu.li){
+//     sideMenu.style.right ="-200px"
+//   }
+// });
+
+
+// text slider
